1ff52299f5b6d077c676538a6a1d28408c1814eb
llassauce <ludovic@simo.co>
2025-09-16 15:26:42 +0800

    Delete react-native directory

--- react-native/src/services/AlwaysONModule.ts
+++
@@ -1,7 +0,0 @@
-import { NativeModulesProxy } from 'expo-modules-core';
-
-// Import the native module. On web, it will be resolved to AlwaysONModule.web.ts
-// and on native platforms to AlwaysONModule.ts
-import AlwaysONModule from './AlwaysONModule';
-
-export default AlwaysONModule;
